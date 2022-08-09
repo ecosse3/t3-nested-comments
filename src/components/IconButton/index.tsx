3ca0c7829c8e6d2743fea5549fc7074012394af2
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-09 22:10:18 +0200

    feat(components): disable clicking when session not found on IconButton

--- src/components/IconButton/index.tsx
+++ src/components/IconButton/index.tsx
@@ -1,5 +1,6 @@
 import { HeroIcon } from "#types/heroicon";
 import clsx from "clsx";
+import { useSession } from "next-auth/react";
 
 export interface IconButtonProps {
   Icon: HeroIcon;
@@ -12,8 +13,9 @@ export interface IconButtonProps {
 
 export const IconButton = (props: IconButtonProps) => {
   const { Icon, isActive, color, children, hoverBg } = props;
+  const { data: session } = useSession();
 
   return (
-    <button className={clsx('bg-none p-1 flex items-center rounded hover:bg-purple-50 focus:outline-purple-400', color, hoverBg, isActive && 'bg-slate-200')} {...props}><Icon className={clsx('h-4 w-4', !isActive && color, isActive && 'text-black', children?.toString() && 'mr-1')} /><span className="text-sm">{children}</span></button>
+    <button className={clsx('bg-none p-1 flex items-center rounded focus:outline-purple-400', color, hoverBg, isActive && 'bg-slate-200', session ? 'hover:bg-purple-50 cursor-pointer' : 'cursor-default')} {...props}><Icon className={clsx('h-4 w-4', !isActive && color, isActive && 'text-black', children?.toString() && 'mr-1')} /><span className="text-sm">{children}</span></button>
   );
 };
