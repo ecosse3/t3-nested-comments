d7bb35fee08fb9f680fc3e5aea28fbecc1692386
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-09 14:17:01 +0200

    chore: remove example router

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,9 @@
 import { createRouter } from "./context";
 import superjson from "superjson";
 
-import { exampleRouter } from "./example";
-import { protectedExampleRouter } from "./protected-example-router";
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge("example.", exampleRouter)
-  .merge("question.", protectedExampleRouter);
 
 // export type definition of API
 export type AppRouter = typeof appRouter;
