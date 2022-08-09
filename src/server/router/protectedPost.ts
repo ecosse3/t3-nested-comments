8fb052769ae0f6add77692adea45506ff4eb7b4c
ecosse3 <luk.kurpiewski@gmail.com>
2022-08-09 14:25:01 +0200

    feat(server): add updateComment mutation

--- src/server/router/protectedPost.ts
+++ src/server/router/protectedPost.ts
@@ -20,3 +20,29 @@ export const protectedPostRouter = createProtectedRouter()
       })
     }
   })
+  .mutation("updateComment", {
+    input: z.object({
+      commentId: z.string(),
+      message: z.string(),
+      postId: z.string(),
+    }),
+    async resolve({ input, ctx }) {
+      const res = await ctx.prisma.comment.findUnique({
+        where: { id: input.commentId },
+        select: { userId: true }
+      });
+
+      if (res?.userId !== ctx.session.user.id) {
+        throw new trpc.TRPCError({ code: 'UNAUTHORIZED', message: "You do not have permission to update this comment" })
+      }
+
+      return await ctx.prisma.comment.update({
+        where: {
+          id: input.commentId
+        },
+        data: {
+          message: input.message,
+        }
+      })
+    }
+  })
